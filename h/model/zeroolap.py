50ad5e022659efde68ce66c602d285d1af3e0320
xayam <xayam@yandex.ru>
2024-08-07 11:28:10 +0300

    v0.22

---
+++ h/model/zeroolap.py
@@ -0,0 +1,93 @@
+import math
+import pprint
+import sys
+from functools import reduce
+
+
+def zero_limit(width=32) -> int:
+    assert width > 0
+    limit = width
+    while True:
+        old_limit = limit
+        limit = 4 * math.ceil(math.log2(limit)) + 2
+        if limit == old_limit:
+            break
+    return limit
+
+
+def zero_get_path_by_name(name: int = 0, width: int = 32) -> list:
+    limit = zero_limit(width)
+    # print(width, name)
+    assert name >= 0
+    passenger = name
+    paths = [0]
+    destination = passenger
+    if passenger == 0:
+        paths.append(limit)
+        return paths
+    else:
+        if passenger == limit:
+            pass
+        else:
+            paths = [destination]
+    while True:
+        destination = 4 * math.ceil(math.log2(destination)) + 2
+        paths.append(destination)
+        if destination == limit:
+            break
+    return paths
+
+
+def zero_get_new_name(old_name: int, width=32) -> list:
+    return [old_name] + zero_get_path_by_name(old_name, width)
+
+
+def zero_paths(name: int = 0, width: int = 32, verbose=0) -> list:
+    assert width > 0
+    assert 0 <= name <= width
+    result = []
+    if name == 0:
+        for i in range(width):
+            paths = zero_get_new_name(i)
+            len_paths = len(paths)
+            len_set_paths = len(set(paths))
+            assert len_paths - len_set_paths == 1
+            result.append(paths)
+        if verbose > 0:
+            pprint.pprint(result)
+        assert len(result) == width
+        return result
+    else:
+        paths = zero_get_new_name(name)
+        len_paths = len(paths)
+        len_set_paths = len(set(paths))
+        if verbose > 0:
+            print(paths, len_paths, len_set_paths)
+        assert len_paths - len_set_paths == 1
+        return paths
+
+
+def main(maximum: int):
+    result = []
+    for width in range(1, maximum):
+        limit = zero_limit(width)
+        paths = zero_paths(width=width, verbose=0)
+        result.append(paths)
+        assert paths
+    pprint.pprint(result, width=len(result[-1]) ** 2 - len(result[-1][-1]) ** 2)
+        # print(paths)
+        # if type(paths[0]) is list:
+            # (paths[-1][-1] == limit):
+            # (len(paths[0]) > 2) and \
+            # (paths[0][0] == 0) and \
+            # assert len(paths[0]) - len(set(paths[0])) == 1
+            # continue
+        # if not type(paths[0]) is list:
+        #     assert len(paths) - len(set(paths)) == 1
+
+
+
+if __name__ == "__main__":
+    main(maximum=64)
+
+    # zero_paths(name=0, width=32)
