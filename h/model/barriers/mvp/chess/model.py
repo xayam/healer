55dee60e97b90562f0a138e055c20bcb0949640d
xayam <xayam@yandex.ru>
2024-08-24 21:38:23 +0300

    v0.161 model

--- h/model/barriers/mvp/chess/model.py
+++ h/model/barriers/mvp/chess/model.py
@@ -2,6 +2,7 @@ import pickle
 import datetime
 
 from kan import *
+import chess.engine
 import chess
 import chess.syzygy
 
@@ -10,14 +11,13 @@ from helpers import poplsb, lsb
 
 
 class Model:
-
     COUNT_FEN_LIMIT = 5000
 
     def __init__(self):
         self.model = None
         self.dataset = None
         self.last_fen = None
-        self.len_input = 11
+        self.len_input = 772
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.dtype = torch.get_default_dtype()
         print(self.device, self.dtype)
@@ -33,22 +33,23 @@ class Model:
         except FileNotFoundError:
             pass
 
-        self.train()
+        # self.train()
         # self.test_model()
         # self.predict()
+        self.search_params()
 
     def train(self):
         results = []
         while True:
             self.dataset = self.get_data(
-                fen_generator=self.fen_generator,
-                get_score=self.get_wdl,
-                limit=1000
+                fen_generator=self.fen_db,
+                get_score=self.get_score,
+                limit=5000
             )
             result = self.model.fit(self.dataset,
                                     lamb=0.01, lamb_entropy=10,
                                     metrics=(self.train_acc, self.test_acc),
-                                    steps=50)
+                                    steps=5)
             print(result['train_acc'][-1], result['test_acc'][-1])
             results.append([
                 result['test_loss'][0] + result['test_loss'][1]
@@ -57,14 +58,56 @@ class Model:
             utils_progress(f"results[-1][-1]={results[-1][-1]}")
             print("Saving ckpt...")
             self.model.saveckpt("./ckpts/wdl_model_" +
-                                str(datetime.datetime.now()).\
-                                replace(":", "-").replace(".", "-"). \
+                                str(datetime.datetime.now()).
+                                replace(":", "-").replace(".", "-").
                                 replace(" ", "-").replace("-", "_"))
             lib = ['x', 'x^2', 'x^3', 'x^4', 'exp', 'log', 'sqrt', 'tanh', 'sin', 'tan', 'abs']
             self.model.auto_symbolic(lib=lib)
             formula = self.model.symbolic_formula()[0][0]
             with open(f"wdl_formula_0.txt", encoding="UTF-8", mode="w") as p:
                 p.write(str(formula).strip())
+            break
+
+    def search_params(self):
+        self.dataset = self.get_data(
+            fen_generator=self.fen_db,
+            get_score=self.get_score,
+            limit=500
+        )
+        results = []
+        maxi = -10 ** 10
+        maximum_layer = -10 ** 10
+        maximum_grid = -10 ** 10
+        maximum_k = -10 ** 10
+        while True:
+            hidden_layer = random.choice(list(range(2, 1000)))
+            grid = random.choice(list(range(5, 30)))
+            k = random.choice(list(range(3, 4)))
+            self.model = KAN(width=[self.len_input, hidden_layer, 1],
+                             grid=grid, k=k,
+                             auto_save=False,
+                             seed=42
+                             )
+            result = self.model.fit(self.dataset,
+                                    lamb=0.00,
+                                    # lamb_entropy=10,
+                                    metrics=(self.train_acc, self.test_acc),
+                                    steps=2)
+            # print(result['test_loss'])
+            results.append([hidden_layer, grid, k,
+                            result['test_loss'][0]
+            ])
+            if result['test_acc'][0] > maxi:
+                maxi = result['test_acc'][0]
+                maximum_layer = hidden_layer
+                maximum_grid = grid
+                maximum_k = k
+            print()
+            print(f"hidden_layer={maximum_layer}, grid={maximum_grid}, k={maximum_k}, " +
+                  f"{result['train_acc'][0]}, {result['test_acc'][0]} " +
+                  f"{maxi}"
+                  )
+            print(f"hidden_layer={hidden_layer}, grid={grid}, k={k}, {results[-1][-1]}")
 
     def train_acc(self):
         return torch.mean((torch.round(self.model(self.dataset['train_input'])[:, 0]) ==
@@ -75,16 +118,28 @@ class Model:
                            self.dataset['test_label'][:, 0]).type(self.dtype))
 
     def get_train(self, state):
-        train_input = [[0., 0.] for _ in range(64)]
-        for color in [False, True]:
-            occupied = state.occupied_co[color]
-            while occupied:
-                square = lsb(occupied)
-                piece = state.piece_type_at(square)
-                train_input[square][0] = int(color)
-                train_input[square][1] = piece
-                occupied = poplsb(occupied)
+        train_input = [[0.] * 64 for _ in range(12)]
+        for piece in chess.PIECE_TYPES:
+            for square in state.pieces(piece, chess.BLACK):
+                train_input[piece - 1][square] = -piece
+                for move in state.pseudo_legal_moves:
+                    if move.from_square == square:
+                        train_input[piece - 1][move.to_square] = -piece
+        for piece in chess.PIECE_TYPES:
+            for square in state.pieces(piece, chess.WHITE):
+                train_input[piece + 5][square] = piece
+                for move in state.pseudo_legal_moves:
+                    if move.from_square == square:
+                        train_input[piece + 5][move.to_square] = piece
         train_input = [j for i in train_input for j in i]
+        if state.has_kingside_castling_rights(state.turn):
+            train_input = [1.] + train_input
+        else:
+            train_input = [0.] + train_input
+        if state.has_kingside_castling_rights(state.turn):
+            train_input = [1.] + train_input
+        else:
+            train_input = [0.] + train_input
         if state.ep_square is None:
             train_input = [0.] + train_input
         else:
@@ -99,27 +154,24 @@ class Model:
         train_labels = []
         test_inputs = []
         test_labels = []
-        r = random.choice([0, 1])
+        # r = random.choice([0, 1])
         for endgame in fen_generator(get_score, limit):
             for fen in endgame:
                 score = get_score(fen)
                 if score is None:
                     continue
                 count += 1
-                self.last_fen = fen
                 utils_progress(f"{str(count).rjust(9, ' ')} | " +
-                               f"{str(get_score(fen)).rjust(2, ' ')} | {fen}")
+                               f"{str(score).rjust(2, ' ')} | {fen}")
                 board = chess.Board()
                 board.set_fen(fen)
                 train_input = self.get_train(state=board)
-                if count % 2 == r:
+                if count % 2 == 0:
                     test_inputs.append(train_input)
                     test_labels.append([score])
                 else:
                     train_inputs.append(train_input)
                     train_labels.append([score])
-                if count % 2 == 0:
-                    r = random.choice([0, 1])
         min_len = min(len(test_inputs), len(train_inputs))
         test_inputs = test_inputs[:min_len]
         test_labels = test_labels[:min_len]
@@ -157,6 +209,34 @@ class Model:
             state.set_piece_at(sq, chess.Piece.from_symbol(piece))
             break
         return state
+
+    @staticmethod
+    def get_score(fen, depth=10):
+        str_stockfish = 'D:/Work2/PyCharm/SmartEval2/github/src/poler/poler/bin' + \
+                        '/stockfish-windows-x86-64-avx2.exe'
+        state = chess.Board()
+        state.set_fen(fen)
+        with chess.engine.SimpleEngine.popen_uci(str_stockfish) as sf:
+            result = sf.analyse(state, chess.engine.Limit(depth=depth))
+            score = result['score'].white().score()
+            return score
+
+
+    def fen_db(self, get_score, limit):
+        with open("dataset.epdeval", mode="r") as f:
+            dataevals = f.readlines()
+        count = 0
+        fens = []
+        for _ in range(limit):
+            count += 1
+            dataeval = str(random.choice(dataevals)).strip()
+            spl = dataeval.split(" ")
+            fen = " ".join(spl[:-1])
+            fens.append(fen)
+            if len(fens) == 2:
+                yield fens
+                fens = []
+
 
     def fen_generator(self, get_score, limit):
         board = chess.Board()
@@ -234,9 +314,9 @@ class Model:
         self.dataset = self.get_data(
             fen_generator=self.fen_generator,
             get_score=self.get_wdl,
-            limit=50
+            limit=100
         )
-        fen = '8/K7/8/8/2k5/8/8/3r4 b - - 0 1'
+        fen = list(self.fen_db(get_score=self.get_score, limit=2))[0][0]
         board = chess.Board()
         board.set_fen(fen)
         inp = self.get_train(state=board)
@@ -245,19 +325,14 @@ class Model:
             f"x_{i}": int(inp[i - 1])
             for i in range(self.len_input, 0, -1)
         }
-        # print(variable_values)
-        # sys.exit()
-        result = self.model.fit(self.dataset, steps=2)
-        print(result['test_loss'][0] + result['test_loss'][1])
-        lib = ['x', 'x^2', 'x^3', 'x^4', 'exp', 'log', 'sqrt', 'tanh', 'sin', 'tan', 'abs']
-        self.model.auto_symbolic(lib=lib)
-        formula = self.model.symbolic_formula()[0][0]
+        with open(f"wdl_formula_0.txt", encoding="UTF-8", mode="r") as p:
+            formula = p.read()
         print(formula)
         for var, val in variable_values.items():
             formula = str(formula).replace(var, str(val))
         result = eval(formula)
         print(result)
-        print(fen)
+        print(fen, self.get_score(fen))
 
 
 if __name__ == "__main__":
