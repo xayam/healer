3f27125bb1e9b1d98cb2619f49ca0a4dc4df1907
xayam <xayam@yandex.ru>
2024-08-31 09:12:56 +0300

    v0.233

--- h/model/barriers/cpu8.py
+++ h/model/barriers/cpu8.py
@@ -1,4 +1,6 @@
 import math
+import random
+import winsound
 
 
 class CPU8:
@@ -10,14 +12,19 @@ class CPU8:
             round(self.limit * 2 ** (i / self.n))
             for i in range(0, self.n + 1)
         ]
+        self.input = []
+        self.state = {}
+        self.clear()
+
+    def clear(self):
         self.input = [0] * 8
         self.state = {pos: -1 for pos in range(self.limit + 1)}
 
-    def get(self, raw, pos):
-        return self.process(value=raw, time=pos)
+    def get(self, raw, seek):
+        return self.process(value=raw, time=seek)
 
-    def process(self, value: int, time: int):
-        s = f"{value}:8b".rjust(8, "0")
+    def process(self, value: int, time: int) -> int:
+        s = f"{value:8b}".replace(" ", "0")
         i = 0
         for c in s:
             self.input[i] = -1 if int(c) == 0 else 1
@@ -29,9 +36,11 @@ class CPU8:
             while t != 0:
                 if pos == self.limit:
                     direction = 1
+                    pos += direction
                     continue
                 elif pos == 2 * self.limit:
                     direction = -1
+                    pos += direction
                     continue
                 pos += direction
                 t -= 1
@@ -50,4 +59,16 @@ class CPU8:
                   self.state[self.freq[4] - self.limit]
         result *= self.state[self.freq[0] - self.limit] * \
                   self.state[self.freq[-1] - self.limit]
+        self.clear()
         return result
+
+if __name__ == "__main__":
+    limit = 432//2
+    cpu = CPU8(limit=limit)
+    rnd = random.SystemRandom(0)
+    for time in range(1024):
+        data = rnd.choice(list(range(256)))
+        r = cpu.get(raw=data, seek=time)
+        print(f"raw={data}, seek={time}, result={r}")
+        if r == -1:
+            winsound.Beep(432, 5)
